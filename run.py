858e5d4ddd4d6a76b803b95e27abfd79a36f0ff2
ascalva <ascalva@gmail.com>
2019-03-08 15:23:10 -0500

    Added routines file with set of function to help automate A-matrix creation

--- run.py
+++ run.py
@@ -1,52 +1,22 @@
 import pandas as pd
 
-from src.create import create_matrix_A, create_matrix_B
-from src.eigs import remove_inf_eigs, save_matrix_to_ml
 from src.preprocess import bound, negate
-
-def k2_routine(df, limits, filename = "sparse_matrices", dirname = "out"):
-
-    B = create_matrix_B(df.shape[0])
-
-    for k_1 in range(limits[0], limits[1]):
-        for k_2 in range(limits[0], limits[1]):
-
-            A      = create_matrix_A(df, [k_1, k_2])
-            F, G_B = remove_inf_eigs(A, B)
-
-            # Save sparse matrices to matlab file for later computation
-            # of eigenvalues.
-            save_matrix_to_ml(
-                F, G_B,
-                "{0}/{1}_{2}_{3}".format(dirname, filename, k_1, k_2)
-            )
+from src.routines import run_range_2k
 
 def main():
 
     # Create data frame with simulation data.
     # out.csv is pre-filtered data from a 90-degree bend flow.
     # Simulation properties include: De = 5, delta = 1e-2, shear banding
-    # filename = "./data/flow_data.csv"
     filename = "data/OB_crossslot_symmetric_De0.37.csv"
     df       = pd.read_csv(filename)
 
     # Clean up data
     df = bound(df)
 
-    # K-values
-    # k = [1,2]
-
-    # Build pre-configured A,B matrices with simulation data
-    # A  = create_matrix_A(df, k)
-    # B  = create_matrix_B(df.shape[0])
-
-
-    # Remove infinite eigen values (map them to smaller values) and save the
-    # resulting matrices as sparse matrices in a matlab file.
-    # remove_inf_eigs(A, B, save_matrix = True)
-
+    # Create A matrices for different values of k
     k_minits = (1,3)
-    k2_routine(df, k_minits)
+    run_range_2k(df, k_minits)
 
 
 main()
