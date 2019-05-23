2cbe366b24c21a961d079e0852b6c0042f8c5fb3
ascalva <ascalva@gmail.com>
2019-05-23 14:28:50 -0400

    Added boundary equations

--- src/equations/boundary_equations.py
+++ src/equations/boundary_equations.py
@@ -19,13 +19,22 @@ def get_inner_corners():
     ]
 
 # points on a boundary:
-# eq_var_wall-orientation_channel_neighbor
+# <eq>_<var>_<wall-orientation>_<channel-type>_<neighbor-location>
 
 # Left wall along left inlet
 def x1_v1_left_inlet_cen(prms):
     return 1
 
+def x2_v2_left_inlet_cen(prms):
+    return 1
+
 def A11_A11_left_inlet_cen(prms):
+    return 1
+
+def A12_A12_left_inlet_cen(prms):
+    return 1
+
+def A22_A22_left_inlet_cen(prms):
     return 1
 
 def m_p_left_inlet_cen(prms):
@@ -34,51 +43,403 @@ def m_p_left_inlet_cen(prms):
 def m_p_left_inlet_right(prms):
     return 1
 
+
 # Right wall along right inlet
-def x1_v1_right_inlet(prms):
+def x1_v1_right_inlet_cen(prms):
     return 1
 
-def A11_A11_right_inlet(prms):
+def x2_v2_right_inlet_cen(prms):
     return 1
 
-def m_p_right_inlet(prms):
+def A11_A11_right_inlet_cen(prms):
     return 1
+
+def A12_A12_right_inlet_cen(prms):
+    return 1
+
+def A22_A22_right_inlet_cen(prms):
+    return 1
+
+def m_p_right_inlet_cen(prms):
+    return -1
+
+def m_p_right_inlet_left(prms):
+    return 1
+
 
 # Upper wall along the top outlet
+def m_p_up_outlet_cen(prms):
+    return 1
+
+def x1_v1_up_outlet_cen(prms):
+    return -1
+
+def x1_v1_up_outlet_down(prms):
+    return 1
+
+def x2_v2_up_outlet_cen(prms):
+    return -1
+
+def x2_v2_up_outlet_down(prms):
+    return 1
+
+def A11_A11_up_outlet_cen(prms):
+    return -1
+
+def A11_A11_up_outlet_down(prms):
+    return 1
+
+def A12_A12_up_outlet_cen(prms):
+    return -1
+
+def A12_A12_up_outlet_down(prms):
+    return 1
+
+def A22_A22_up_outlet_cen(prms):
+    return -1
+
+def A22_A22_up_outlet_down(prms):
+    return 1
 
 
 # Bottom wall along the bottom outlet
+def m_p_down_outlet_cen(prms):
+    return 1
+
+def x1_v1_down_outlet_cen(prms):
+    return -1
+
+def x1_v1_down_outlet_up(prms):
+    return 1
+
+def x2_v2_down_outlet_cen(prms):
+    return -1
+
+def x2_v2_down_outlet_up(prms):
+    return 1
+
+def A11_A11_down_outlet_cen(prms):
+    return -1
+
+def A11_A11_down_outlet_up(prms):
+    return 1
+
+def A12_A12_down_outlet_cen(prms):
+    return -1
+
+def A12_A12_down_outlet_up(prms):
+    return 1
+
+def A22_A22_down_outlet_cen(prms):
+    return -1
+
+def A22_A22_down_outlet_up(prms):
+    return 1
 
 
 # The top walls along the inlets
+def x1_v1_down_inlet_cen(prms):
+    return 0
+
+def x2_v2_down_inlet_cen(prms):
+    return 0
+
+def m_p_down_inlet_cen(prms):
+    return 1
+
+def m_p_down_inlet_up(prms):
+    return -1
+
+def A11_A11_down_inlet_cen(prms):
+    return 1
+
+def A11_A11_down_inlet_up(prms):
+    return -1
+
+def A12_A12_down_inlet_cen(prms):
+    return 1
+
+def A12_A12_down_inlet_up(prms):
+    return -1
+
+def A22_A22_down_inlet_cen(prms):
+    return 1
+
+def A22_A22_down_inlet_up(prms):
+    return -1
 
 # The bottom walls along the inlets
+def x1_v1_up_inlet_cen(prms):
+    return 0
+
+def x2_v2_up_inlet_cen(prms):
+    return 0
+
+def m_p_up_inlet_cen(prms):
+    return -1
+
+def m_p_up_inlet_down(prms):
+    return 1
+
+def A11_A11_up_inlet_cen(prms):
+    return -1
+
+def A11_A11_up_inlet_down(prms):
+    return 1
+
+def A12_A12_up_inlet_cen(prms):
+    return -1
+
+def A12_A12_up_inlet_down(prms):
+    return 1
+
+def A22_A22_up_inlet_cen(prms):
+    return -1
+
+def A22_A22_up_inlet_down(prms):
+    return 1
+
 
 # The left walls along the outlets
+def x1_v1_left_outlet_cen(prms):
+    return 0
+
+def x2_v2_left_outlet_cen(prms):
+    return 0
+
+def m_p_left_outlet_cen(prms):
+    return -1
+
+def m_p_left_outlet_right(prms):
+    return 1
+
+def A11_A11_left_outlet_cen(prms):
+    return -1
+
+def A11_A11_left_outlet_right(prms):
+    return 1
+
+def A12_A12_left_outlet_cen(prms):
+    return -1
+
+def A12_A12_left_outlet_right(prms):
+    return 1
+
+def A22_A22_left_outlet_cen(prms):
+    return -1
+
+def A22_A22_left_outlet_right(prms):
+    return 1
+
 
 # The right walls along the outlets
+def x1_v1_right_outlet_cen(prms):
+    return 0
 
-# Outer corners along the outlets
+def x2_v2_right_outlet_cen(prms):
+    return 0
 
-# Outer corners along the inlets
+def m_p_right_outlet_cen(prms):
+    return 1
 
-# def A11_A11_bound(prms):
-#     return 1
-#
-# def A12_A12_bound(prms):
-#     return 1
-#
-# def A22_A22_bound(prms):
-#     return 1
-#
-# def x1_v1_bound(prms):
-#     return 1
-#
-# def x2_v2_bound(prms):
-#     return 1
-#
-# def m_p_bound(prms):
-#     return 1
-#
-# def inner_corner_bound(prms):
-#     return 1
+def m_p_right_outlet_left(prms):
+    return -1
+
+def A11_A11_right_outlet_cen(prms):
+    return 1
+
+def A11_A11_right_outlet_left(prms):
+    return -1
+
+def A12_A12_right_outlet_cen(prms):
+    return 1
+
+def A12_A12_right_outlet_left(prms):
+    return -1
+
+def A22_A22_right_outlet_cen(prms):
+    return 1
+
+def A22_A22_right_outlet_left(prms):
+    return -1
+
+### Outer corners along the outlets ###
+
+# Up-left corner
+def m_p_up_left_outlet_cen(prms):
+    return 1
+
+def x1_v1_up_left_outlet_cen(prms):
+    return -1
+
+def x1_v1_up_left_outlet_down(prms):
+    return 1
+
+def x2_v2_up_left_outlet_cen(prms):
+    return -1
+
+def x2_v2_up_left_outlet_down(prms):
+    return 1
+
+def A11_A11_up_left_outlet_cen(prms):
+    return 0
+
+def A12_A12_up_left_outlet_cen(prms):
+    return 0
+
+def A22_A22_up_left_outlet_cen(prms):
+    return 0
+
+# Up-right corner
+def m_p_up_right_outlet_cen(prms):
+    return 1
+
+def x1_v1_up_right_outlet_cen(prms):
+    return -1
+
+def x1_v1_up_right_outlet_down(prms):
+    return 1
+
+def x2_v2_up_right_outlet_cen(prms):
+    return -1
+
+def x2_v2_up_right_outlet_down(prms):
+    return 1
+
+def A11_A11_up_right_outlet_cen(prms):
+    return 0
+
+def A12_A12_up_right_outlet_cen(prms):
+    return 0
+
+def A22_A22_up_right_outlet_cen(prms):
+    return 0
+
+# Down-right corner
+def m_p_down_right_outlet_cen(prms):
+    return 1
+
+def x1_v1_down_right_outlet_cen(prms):
+    return -1
+
+def x1_v1_down_right_outlet_up(prms):
+    return 1
+
+def x2_v2_down_right_outlet_cen(prms):
+    return -1
+
+def x2_v2_down_right_outlet_up(prms):
+    return 1
+
+def A11_A11_down_right_outlet_cen(prms):
+    return 0
+
+def A12_A12_down_right_outlet_cen(prms):
+    return 0
+
+def A22_A22_down_right_outlet_cen(prms):
+    return 0
+
+# Down-left corner
+def m_p_down_left_outlet_cen(prms):
+    return 1
+
+def x1_v1_down_left_outlet_cen(prms):
+    return -1
+
+def x1_v1_down_left_outlet_up(prms):
+    return 1
+
+def x2_v2_down_left_outlet_cen(prms):
+    return -1
+
+def x2_v2_down_left_outlet_up(prms):
+    return 1
+
+def A11_A11_down_left_outlet_cen(prms):
+    return 0
+
+def A12_A12_down_left_outlet_cen(prms):
+    return 0
+
+def A22_A22_down_left_outlet_cen(prms):
+    return 0
+
+
+### Outer corners along the inlets ###
+
+# Up-left corner
+def m_p_up_left_inlet_cen(prms):
+    return 0
+
+def x1_v1_up_left_inlet_cen(prms):
+    return 1
+
+def x2_v2_up_left_inlet_cen(prms):
+    return 1
+
+def A11_A11_up_left_inlet_cen(prms):
+    return 1
+
+def A12_A12_up_left_inlet_cen(prms):
+    return 1
+
+def A22_A22_up_left_inlet_cen(prms):
+    return 1
+
+# Up-right corner
+def m_p_up_right_inlet_cen(prms):
+    return 0
+
+def x1_v1_up_right_inlet_cen(prms):
+    return 1
+
+def x2_v2_up_right_inlet_cen(prms):
+    return 1
+
+def A11_A11_up_right_inlet_cen(prms):
+    return 1
+
+def A12_A12_up_right_inlet_cen(prms):
+    return 1
+
+def A22_A22_up_right_inlet_cen(prms):
+    return 1
+
+# Down-right corner
+def m_p_down_right_inlet_cen(prms):
+    return 0
+
+def x1_v1_down_right_inlet_cen(prms):
+    return 1
+
+def x2_v2_down_right_inlet_cen(prms):
+    return 1
+
+def A11_A11_down_right_inlet_cen(prms):
+    return 1
+
+def A12_A12_down_right_inlet_cen(prms):
+    return 1
+
+def A22_A22_down_right_inlet_cen(prms):
+    return 1
+
+# Down-left corner
+def m_p_down_left_inlet_cen(prms):
+    return 0
+
+def x1_v1_down_left_inlet_cen(prms):
+    return 1
+
+def x2_v2_down_left_inlet_cen(prms):
+    return 1
+
+def A11_A11_down_left_inlet_cen(prms):
+    return 1
+
+def A12_A12_down_left_inlet_cen(prms):
+    return 1
+
+def A22_A22_down_left_inlet_cen(prms):
+    return 1
